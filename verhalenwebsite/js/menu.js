bbaa4cb57b7fb9fd0cac761f89d06d5a784821b3
Joopakerboom <31732324+Joopakerboom@users.noreply.github.com>
2017-09-21 13:13:06 +0200

    Verder gegaan met html en ccs
    Html is nu correct en begonnen aan css.

---
+++ verhalenwebsite/js/menu.js
@@ -0,0 +1,4 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+document.getElementById("openmenuicon").addEventListener("click", openMenu);
+document.getElementById("closemenuicon").addEventListener("click", closeMenu);
