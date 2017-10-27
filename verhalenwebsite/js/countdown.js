580e0c41ad17c830652deea037b908b071dfe1ca
Joopakerboom <31732324+Joopakerboom@users.noreply.github.com>
2017-10-27 15:35:50 +0200

    Sort and filter gestyled en Counter
    Naast het stijlen, ook responsive gemaakt. Verder de counter en time picker begonnen.

--- verhalenwebsite/js/countdown.js
+++ verhalenwebsite/js/countdown.js
@@ -1,1 +1,17 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+var timeoutHandle;
+    function count() {
+
+    var startTime = document.getElementById('hms').innerHTML;
+    var pieces = startTime.split(":");
+    var time = new Date();    time.setHours(pieces[0]);
+    time.setMinutes(pieces[1]);
+    time.setSeconds(pieces[2]);
+    var timedif = new Date(time.valueOf() - 1000);
+    var newtime = timedif.toTimeString().split(" ")[0];
+    document.getElementById('hms').innerHTML=newtime;
+    timeoutHandle=setTimeout(count, 1000);
+}
+count();
+
---
+++ verhalenwebsite/js/sortfilter.js
@@ -0,0 +1,13 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/* 1. selecteer elementen met document.querySelector */
+var section = document.querySelector('form.dancer'); // first section element
+var button = document.getElementById("kak");
+
+var dance = function () {
+  /* 3. toggle een class op een element, dit triggert een CSS Transition (zie CSS: section.dance) */
+	section.classList.toggle('swag');
+};
+
+/* 2. voeg een event toe aan een element */
+button.addEventListener('click', dance);
